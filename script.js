b0dd2bdef16149fc2d154546f961d46f952ad3f3
Adebayo <adenguk5251@gmail.com>
2024-03-21 12:12:18 +0000

    added 5-day format html

---
+++ script.js
@@ -0,0 +1,32 @@
+let cityInput = document.querySelector("#city-input");
+let searchButton = document.querySelector(".search-btn");
+let currentWeatherDiv = document.querySelector(".current-weather");
+let weatherCardsDiv = document.querySelector(".weather-cards");
+let searched = document.querySelector("#searchdata");
+let searchHistory = (localStorage.searchHistory) ? JSON.parse(localStorage.searchHistory) : [];
+
+const API_KEY = "249281e6cc79f719d0132c7ea0c3b402";
+
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if(index === 0) { 
+        return `<div class="details">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>${weatherItem.weather[0].description}</h6>
+                </div>`;
+    }
+    else { 
+        return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </li>`;
+    }
+}
